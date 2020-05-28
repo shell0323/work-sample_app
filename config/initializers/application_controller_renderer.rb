7b884e92942152497abafa46f6119528be80a253
vagrant <vagrant@localhost.localdomain>
2020-05-28 01:45:09 +0000

    [update] second commit

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '3323602856648148443ecf2e04823c1f54d500532e0937ebfa95cb110c4415a5a2884a46e90ae0ce2f304ca2c78e4bb804637f24df1b4df176a3e397797cb67a'
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'todolists/new'
   get 'top' => 'homes#top'
+
   post 'todolists' => 'todolists#create'
 
   get 'todolists' => 'todolists#index'
@@ -11,4 +12,6 @@ Rails.application.routes.draw do
   get 'todolists/:id/edit' => 'todolists#edit', as:'edit_todolist'
 
   patch 'todolists/:id' => 'todolists#update', as:'update_todolist'
+
+  delete 'todolists/:id' => 'todolists#destroy', as:'destroy_todolist'
 end
